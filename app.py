dff9fe187ed3e2cf73618e3826fc3b9763e37fb4
brassy endomorph <brassy-endomorph@riseup.net>
2024-01-22 07:12:47 +0000

    bind to localhost by default

--- app.py
+++ app.py
@@ -1265,4 +1265,4 @@ initialize_database()
 
 if __name__ == "__main__":
     initialize_database()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
