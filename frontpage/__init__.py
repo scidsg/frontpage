4859654cef73101171d8832002c72dca02a0a7f5
brassy endomorph <brassy-endomorph@riseup.net>
2024-01-22 08:27:28 +0000

    moved forms into own module

--- frontpage/__init__.py
+++ frontpage/__init__.py
@@ -11,15 +11,20 @@ from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_migrate import Migrate
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileField
 from slugify import slugify
 from sqlalchemy.exc import IntegrityError
 from werkzeug.utils import secure_filename
-from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
-from wtforms.validators import URL, DataRequired, EqualTo, Length, Optional, Regexp, ValidationError
 
 from .db import db
+from .forms import (
+    AvatarForm,
+    BioForm,
+    CustomUrlForm,
+    DisplayNameForm,
+    PasswordForm,
+    RegistrationForm,
+    TeamPageForm,
+)
 from .models import Article, ArticleType, Category, InvitationCode, User
 
 load_dotenv()  # Load environment variables from .env file
@@ -40,102 +45,6 @@ migrate = Migrate(app, db)
 # Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField(
-        "Password",
-        validators=[
-            DataRequired(),
-            Length(min=8, message="Password must be at least 8 characters long."),
-            Regexp(r"(?=.*[A-Za-z])", message="Password must contain letters."),
-            Regexp(r"(?=.*[0-9])", message="Password must contain numbers."),
-            Regexp(
-                r"(?=.*[-!@#$%^&*()_+])",
-                message="Password must contain at least one special character (-!@#$%^&*()_+).",
-            ),
-        ],
-    )
-    confirm_password = PasswordField(
-        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
-    )
-    invite_code = StringField("Invite Code", validators=[DataRequired()])
-    submit = SubmitField("Register")
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError("That username is already taken. Please choose a different one.")
-
-    def validate_invite_code(self, invite_code):
-        code = InvitationCode.query.filter_by(code=invite_code.data, used=False).first()
-        if not code:
-            raise ValidationError("Invalid or expired invite code.")
-
-
-class BioForm(FlaskForm):
-    bio = TextAreaField("Bio", render_kw={"placeholder": "Tell us something about yourself"})
-    submit_bio = SubmitField("Update Bio")
-
-
-class PasswordForm(FlaskForm):
-    current_password = PasswordField(
-        "Current Password",
-        validators=[DataRequired()],
-        render_kw={"placeholder": "Current Password"},
-    )
-    new_password = PasswordField(
-        "New Password",
-        validators=[
-            DataRequired(),
-            Length(min=8, message="Password must be at least 8 characters long."),
-            Regexp(r"(?=.*[A-Za-z])", message="Password must contain letters."),
-            Regexp(r"(?=.*[0-9])", message="Password must contain numbers."),
-            Regexp(
-                r"(?=.*[-!@#$%^&*()_+])",
-                message="Password must contain at least one special character (-!@#$%^&*()_+).",
-            ),
-        ],
-        render_kw={"placeholder": "New Password"},
-    )
-    confirm_new_password = PasswordField(
-        "Confirm New Password",
-        validators=[
-            DataRequired(),
-            EqualTo("new_password", message="Passwords must match."),
-        ],
-        render_kw={"placeholder": "Confirm New Password"},
-    )
-    submit_password = SubmitField("Update Password")
-
-
-class DisplayNameForm(FlaskForm):
-    display_name = StringField(
-        "Display Name",
-        validators=[Length(max=100)],
-        render_kw={"placeholder": "Enter a display name"},
-    )
-    submit_display_name = SubmitField("Update Display Name")
-
-
-class TeamPageForm(FlaskForm):
-    include_in_team_page = BooleanField("Include in Team Page", default=False)
-    submit_team = SubmitField("Update Team Setting")
-
-
-class CustomUrlForm(FlaskForm):
-    custom_url = StringField(
-        "Custom URL",
-        validators=[Optional(), URL(message="Must be a valid URL.")],
-        render_kw={"placeholder": "Enter your custom URL"},
-    )
-    submit_custom_url = SubmitField("Update Custom URL")
-
-
-class AvatarForm(FlaskForm):
-    avatar = FileField("Avatar", validators=[FileAllowed(["jpg", "png"], "Images only!")])
-    submit_avatar = SubmitField("Upload Avatar")
 
 
 @app.errorhandler(404)
---
+++ frontpage/forms.py
@@ -0,0 +1,102 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
+from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
+from wtforms.validators import URL, DataRequired, EqualTo, Length, Optional, Regexp, ValidationError
+
+from .models import InvitationCode, User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=8, message="Password must be at least 8 characters long."),
+            Regexp(r"(?=.*[A-Za-z])", message="Password must contain letters."),
+            Regexp(r"(?=.*[0-9])", message="Password must contain numbers."),
+            Regexp(
+                r"(?=.*[-!@#$%^&*()_+])",
+                message="Password must contain at least one special character (-!@#$%^&*()_+).",
+            ),
+        ],
+    )
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    invite_code = StringField("Invite Code", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is already taken. Please choose a different one.")
+
+    def validate_invite_code(self, invite_code):
+        code = InvitationCode.query.filter_by(code=invite_code.data, used=False).first()
+        if not code:
+            raise ValidationError("Invalid or expired invite code.")
+
+
+class BioForm(FlaskForm):
+    bio = TextAreaField("Bio", render_kw={"placeholder": "Tell us something about yourself"})
+    submit_bio = SubmitField("Update Bio")
+
+
+class PasswordForm(FlaskForm):
+    current_password = PasswordField(
+        "Current Password",
+        validators=[DataRequired()],
+        render_kw={"placeholder": "Current Password"},
+    )
+    new_password = PasswordField(
+        "New Password",
+        validators=[
+            DataRequired(),
+            Length(min=8, message="Password must be at least 8 characters long."),
+            Regexp(r"(?=.*[A-Za-z])", message="Password must contain letters."),
+            Regexp(r"(?=.*[0-9])", message="Password must contain numbers."),
+            Regexp(
+                r"(?=.*[-!@#$%^&*()_+])",
+                message="Password must contain at least one special character (-!@#$%^&*()_+).",
+            ),
+        ],
+        render_kw={"placeholder": "New Password"},
+    )
+    confirm_new_password = PasswordField(
+        "Confirm New Password",
+        validators=[
+            DataRequired(),
+            EqualTo("new_password", message="Passwords must match."),
+        ],
+        render_kw={"placeholder": "Confirm New Password"},
+    )
+    submit_password = SubmitField("Update Password")
+
+
+class DisplayNameForm(FlaskForm):
+    display_name = StringField(
+        "Display Name",
+        validators=[Length(max=100)],
+        render_kw={"placeholder": "Enter a display name"},
+    )
+    submit_display_name = SubmitField("Update Display Name")
+
+
+class TeamPageForm(FlaskForm):
+    include_in_team_page = BooleanField("Include in Team Page", default=False)
+    submit_team = SubmitField("Update Team Setting")
+
+
+class CustomUrlForm(FlaskForm):
+    custom_url = StringField(
+        "Custom URL",
+        validators=[Optional(), URL(message="Must be a valid URL.")],
+        render_kw={"placeholder": "Enter your custom URL"},
+    )
+    submit_custom_url = SubmitField("Update Custom URL")
+
+
+class AvatarForm(FlaskForm):
+    avatar = FileField("Avatar", validators=[FileAllowed(["jpg", "png"], "Images only!")])
+    submit_avatar = SubmitField("Upload Avatar")
