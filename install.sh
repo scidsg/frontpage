2c37bea2a9bbefdf0ac3356a490eeb5185e71ad8
brassy endomorph <brassy-endomorph@riseup.net>
2024-01-31 07:15:58 +0000

    install script fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
-#Run as root
+# Run as root
 if [[ $EUID -ne 0 ]]; then
   echo "Script needs to run as root. Elevating permissions now."
   exec sudo /bin/bash "$0" "$@"
+  exit $?
 fi
 
 # Welcome message and ASCII art
@@ -21,7 +23,6 @@ https://github.com/scidsg/frontpage
 A free tool by Science & Design - https://scidsg.org
 
 EOF
-sleep 3
 
 # Function to display error message and exit
 error_exit() {
@@ -34,10 +35,10 @@ trap error_exit ERR
 
 # Update and upgrade the system
 export DEBIAN_FRONTEND=noninteractive
-sudo apt update && sudo apt -y dist-upgrade
+apt update && apt -y dist-upgrade
 
 # Install Python and pip
-sudo apt install -y \
+apt install -y \
     git \
     nginx \
     python3 \
@@ -58,6 +59,8 @@ source venv/bin/activate
 # Install Flask and other dependencies
 poetry install --only main
 
+touch .env
+
 # Generate and export Flask secret key
 if ! egrep -q '^FLASK_SECRET_KEY=' .env; then
     echo 'Generating new secret key'
@@ -70,25 +73,25 @@ export FLASK_APP=frontpage:app
 poetry run flask db upgrade
 
 # Change owner and permissions of the SQLite database file
-sudo chown -R www-data:www-data /var/www/html/frontpage
+chown -R www-data:www-data /var/www/html/frontpage
 
 # Add upload directory
-mkdir /var/www/html/frontpage/frontpage/static/uploads
-sudo chown -R www-data:www-data /var/www/html/frontpage/frontpage/static/uploads
-sudo chmod -R 755 /var/www/html/frontpage/frontpage/static/uploads
+mkdir -p /var/www/html/frontpage/frontpage/static/uploads
+chown -R www-data:www-data /var/www/html/frontpage/frontpage/static/uploads
+chmod -R 755 /var/www/html/frontpage/frontpage/static/uploads
 
-sudo cp files/frontpage.service /etc/systemd/system/frontpage.service
+cp files/frontpage.service /etc/systemd/system/frontpage.service
 
 # Install Nginx
-sudo apt install nginx -y
+apt install nginx -y
 
 # Set up Nginx to proxy requests to Flask
-sudo cp files/nginx.conf /etc/nginx/sites-available/frontpage
-sudo ln -s /etc/nginx/sites-available/frontpage /etc/nginx/sites-enabled
+cp files/nginx.conf /etc/nginx/sites-available/frontpage
+ln -sf /etc/nginx/sites-available/frontpage /etc/nginx/sites-enabled
 rm -f /etc/nginx/sites-available/default
 rm -f /etc/nginx/sites-enabled/default
-sudo systemctl reload nginx
+systemctl reload nginx
 
-sudo systemctl enable frontpage.service
-sudo systemctl start frontpage.service
-sudo systemctl restart frontpage.service
+systemctl enable frontpage.service
+systemctl start frontpage.service
+systemctl restart frontpage.service
